013721f22740f725b2cd3b5b092b7cf34f995afb
ampsteric <ampsteric@gmail.com>
2021-02-05 00:25:32 +0530

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [username, setusername] = useState("");
+
+    return (
+        <div className="App">
+            <Router>
+                <Route>
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <form>
+                                        <input
+                                            type="password"
+                                            onChange={(e) => {
+                                                e.preventDefault();
+                                                setusername(e.target.value);
+                                            }}
+                                        />{" "}
+                                        <br />
+                                        <Link to="/panel">
+                                            <button
+                                                class="button"
+                                                type="submit"
+                                            >
+                                                Submit
+                                            </button>
+                                        </Link>
+                                    </form>
+                                </div>
+                                <div className="col-md-12">
+                                    {/* <img src={Octa} style={{ width: "50%" }} /> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Route>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
